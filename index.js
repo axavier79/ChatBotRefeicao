bbedbc65de1994ea41806ff777e0ce78c8b1ca97
Anderson <axavier79@gmail.com>
2020-05-02 13:55:18 -0300

    Iniciando chat bot waterfall

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/** * Author: Anderson Xavier * Data: 02/05/2020 * Descrição: Desenvolvimento de um Bot via Bot Emulator. **/
 //Adicionando os pacotes
 var restify = require('restify');
 var builder = require('botbuilder');
@@ -10,7 +11,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function(){
 
 //Necessário criar uma "custom state client" para o robô, pois o padrão está depreciado
 var inMemoryStorage = new builder.MemoryBotStorage();
-
+ 
 //Configurando o conector para o robô
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -20,7 +21,25 @@ var connector = new builder.ChatConnector({
 //Método POST para receber mensagens do usuário
 server.post('/api/messages', connector.listen());
 
-//Request para o bot interagir com o usuário
-var bot = new builder.UniversalBot(connector, function(session){
-    session.send("Você disse: %s", session.message.text);
-}).set('storage', inMemoryStorage);
+// Bloco de Dialogs: 
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send("Welcome to the dinner reservation.");
+        builder.Prompts.time(session, "Please provide a reservation date and time (e.g.: June 6th at 5pm)");
+    },
+    function (session, results) {
+        session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        builder.Prompts.text(session, "How many people are in your party?");
+    },
+    function (session, results) {
+        session.dialogData.partySize = results.response;
+        builder.Prompts.text(session, "Whose name will this reservation be under?");
+    },
+    function (session, results) {
+        session.dialogData.reservationName = results.response;
+
+        // Process request and display reservation details
+        session.send(`Reservation confirmed. Reservation details: <br/>Date/Time: ${session.dialogData.reservationDate} <br/>Party size: ${session.dialogData.partySize} <br/>Reservation name: ${session.dialogData.reservationName}`);
+        session.endDialog();
+    }
+]).set('storage', inMemoryStorage); // Register in-memory storage 
