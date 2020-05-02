c6ca70e4080fe42cff934dee78e6bda5ef6cacce
Anderson <axavier79@gmail.com>
2020-05-02 11:31:05 -0300

    Pronto para testar após a instalação do Bot Framework Emulator

---
+++ index.js
@@ -0,0 +1,22 @@
+//Adicionando os pacotes
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//Configurando a porta onde o projeto responderá
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function(){
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+//Método POST para receber mensagens do usuário
+server.post('/api/messages', connector.listen());
+
+//Request para o bot interagir com o usuário
+var bot = new builder.UniversalBot(connector, function(session){
+    session.send("Você disse: %s", session.message.text);
+});
