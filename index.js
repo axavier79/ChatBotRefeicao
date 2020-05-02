a58a93f0ec2388db5312bf6a3d2e847f3800fd68
Anderson <axavier79@gmail.com>
2020-05-02 12:00:07 -0300

    botbuilder com Bot Framework Emulator funcionando

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function(){
     console.log('%s listening to %s', server.name, server.url);
 });
 
+//Necessário criar uma "custom state client" para o robô, pois o padrão está depreciado
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+//Configurando o conector para o robô
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -19,4 +23,4 @@ server.post('/api/messages', connector.listen());
 //Request para o bot interagir com o usuário
 var bot = new builder.UniversalBot(connector, function(session){
     session.send("Você disse: %s", session.message.text);
-});
+}).set('storage', inMemoryStorage);
