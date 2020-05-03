89c953c759dd4360d607da2a7211802ff9ccc535
Anderson <axavier79@gmail.com>
2020-05-03 14:01:29 -0300

    Não consegui fazer funcionar a integração do ChatBot com um Chat já existente, utilizando a mesma porta em ambos

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ChatBotRefeicao
 Teste Ebix.
+
+Obs.:
+- Precisa utilizar a versão 3.13.1 do botBuilder (comando: npm i -S botbuilder@3.13.1 para voltar para esta versão, caso esteja com uma versão instalada mais atualizada )
--- app.js
+++ app.js
@@ -18,6 +18,29 @@ const port = 5000;
 var restify = require('restify');
 var builder = require('botbuilder');
 
+//ChatBot
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 5000, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+//Necessário criar uma "custom state client" para o robô, pois o padrão está depreciado
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+//Método POST para receber mensagens do usuário
+server.post('/api/messages', connector.listen());
+
+//Request para o bot interagir com o usuário
+var bot = new builder.UniversalBot(connector, function(session){
+    session.send("Você disse: %s", session.message.text);
+}).set('storage', inMemoryStorage);
+//ChatBot
+
 //bodyparser middleware
 app.use(bodyParser.json());
 
@@ -34,23 +57,6 @@ socket = io(http);
 //database connection
 const Chat = require("./models/Chat");
 const connect = require("./dbconnect");
-
-//ChatBot
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
-
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
-
-server.post('/api/messages', connector.listen());
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("Você disse: %s", session.message.text);
-});
-//ChatBot
 
 //setup event listener
 socket.on("connection", socket => {
